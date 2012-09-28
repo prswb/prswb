357ba662b8ec0e30791b6e87b6d4452e749aec17
Lionel <lionel@soreia.com>
2012-09-28 17:43:06 +0200

    manage contact form and emails

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render_to_response
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.views.decorators.csrf import csrf_protect
+from django.core.mail import send_mass_mail
 from pages.forms import ContactForm
+from django.conf import settings
 
+@csrf_protect
 def contact(request):
-    form = ContactForm()
-    return render_to_response('pages/contact.html', {form: form})
+    """ Contact form
+    Display and proceed contact form submission
+    """
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            send_message(form.cleaned_data)
+            return HttpResponseRedirect('/contact/merci')
+    else:
+        form = ContactForm()
+    return render(request, 'pages/contact.html', {'form': form})
+
+def confirm_contact(request):
+    """ Contact form confirmation """
+    return render(request, 'pages/confirm_contact.html')
+
+def send_message(data):
+    to_sender_message = """
+Bonjour,
+
+Votre commentaire sur le site www.uxperiment.fr a bien été pris en compte.
+
+Nous vous remercions de votre participation.
+
+A bientôt,
+
+L'équipe UXperiment
+"""
+    to_admin_message = 'Emetteur : %s \n %s' % (data['sender'], data['message'])
+    to_sender = ('Commentaire sur UXperiment', to_sender_message, 
+            'no-reply@uxperiment.fr', [data['sender']])
+    to_admin = (data['subject'], to_admin_message, data['sender'], 
+        [settings.EMAIL_RECIPIENT])
+    send_mass_mail((to_sender, to_admin))
