64673c37810f4d8d818de59637c7c31e936c8a3a
Lionel <lionel@soreia.com>
2012-09-28 14:19:19 +0200

    base call for contact form

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render_to_response
+from pages.forms import ContactForm
 
 def contact(request):
-    return render_to_response('pages/contact.html')
+    form = ContactForm()
+    return render_to_response('pages/contact.html', {form: form})
