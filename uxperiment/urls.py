54a3e79fb8d4088896ddbcdc69b6c7af6e2874c7
Stéphane Langlois <stephane.langlois@uuidget.com>
2012-09-29 13:12:32 +0200

    static file serve

--- uxperiment/urls.py
+++ uxperiment/urls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -28,4 +29,15 @@ urlpatterns = patterns('',
         name='markdown_page'),
 )
 
-urlpatterns += staticfiles_urlpatterns()
+#urlpatterns += staticfiles_urlpatterns()
+
+# local dev
+
+urlpatterns += patterns('',
+     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+        }),
+     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+)
