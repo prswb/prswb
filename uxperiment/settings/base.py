65c1a43b75cf14c5c14670504865536a0f0e593c
Lionel <lionel@soreia.com>
2012-09-29 11:05:55 +0200

    static file root

--- uxperiment/settings/base.py
+++ uxperiment/settings/base.py
@@ -1,6 +1,6 @@
 # Django settings for uxperiment project.
 import os.path
-ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -55,7 +55,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.abspath(ROOT_DIR, 'uxperiment', 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
