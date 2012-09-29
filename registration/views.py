4215cab02da7c788d198e65099dd788ba7c7ed90
Lionel <lionel@soreia.com>
2012-09-29 18:59:05 +0200

    registration submission

--- registration/views.py
+++ registration/views.py
@@ -4,5 +4,14 @@ from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render
 
 def signin(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('confirm_signin')
+    else:
+        form = UserCreationForm()
+
     return render(request, 'registration/signin.html', {'form': form})
+
+def confirm_signin(request):
+    return render(request, 'registration/confirm_signin.html')
