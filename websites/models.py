1e5b7b490ea1215532020b241c6ee6ac980998f3
Mathieu Tricoire <mathieu.tricoire@gmail.com>
2012-09-29 20:12:33 +0200

    Add Send message function to prevent new website suggestion

--- websites/models.py
+++ websites/models.py
@@ -2,8 +2,8 @@
 from django.db import models
 
 class Website(models.Model):
-    url         = models.CharField(max_length=255, unique=True)
-    #title       = models.CharFIeld(max_length=255)
-    #description = models.CharFIeld(max_length=255)
-    #image       = models.CharFIeld(max_length=255)
+    url         = models.URLField(max_length=255, unique=True)
+    title       = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    picture     = models.CharField(max_length=255)
 
--- websites/views.py
+++ websites/views.py
@@ -15,7 +15,12 @@ def suggest(request):
     if request.method == 'POST':
         form = SuggestForm(request.POST)
         if form.is_valid():
-            #send_message(form.cleaned_data)
+            website = form.save()
+            send_message(
+                website.url,
+                'TODO authenticated username',
+                settings.EMAIL_RECIPIENT # Replace with user email
+            )
             return HttpResponseRedirect(reverse('confirm_suggest_website'))
     else:
         form = SuggestForm()
@@ -26,5 +31,23 @@ def confirm_suggest(request):
     return render(request, 'websites/confirm_suggest.html')
 
 
-#def send_message(data):
-#
+def send_message(website, username, email):
+    subject = 'Proposition de site internet sur UXperiment'
+    to_sender_message = """
+Bonjour,
+
+Votre proposition de site internet sur le site www.uxperiment.fr a bien été pris en compte.
+
+Nous vous remercions de votre participation.
+
+A bientôt,
+
+L'équipe UXperiment
+"""
+    to_admin_message = 'L''utilisateur : %s, vient de proposer le site %s'\
+        % (username, website)
+    to_sender = (subject, to_sender_message, 'no-reply@uxperiment.fr',
+            [email])
+    to_admin = (subject, to_admin_message, email,
+        [settings.EMAIL_RECIPIENT])
+    send_mass_mail((to_sender, to_admin))
