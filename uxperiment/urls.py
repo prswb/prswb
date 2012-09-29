d91bf6c5430f3cda512ca6b1a4d67e4dbb801558
Lionel <lionel@soreia.com>
2012-09-29 17:07:16 +0200

    add signin url

--- uxperiment/urls.py
+++ uxperiment/urls.py
@@ -31,6 +31,7 @@ urlpatterns = patterns('',
     url(r'^pages/(?P<slug>[-\w\d]+)/$', 'pages.views.markdown_page',
         name='markdown_page'),
     url(r'^compte/', include('django.contrib.auth.urls')),
+    url(r'^compte/creation/$', 'pages.views.signin', name="signin"),
 )
 
 # local dev
