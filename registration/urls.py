517a050e31218a6246218c686031430b9f8c8aa8
Lionel <lionel@soreia.com>
2012-09-29 19:05:25 +0200

    add url for confirm signin

--- registration/urls.py
+++ registration/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('', 
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^creation/$', 'registration.views.signin', name="signin"),
-    url(r'^creation/merci$', 'registration.views.confirm_signin', 
+    url(r'^creation/merci/$', 'registration.views.confirm_signin', 
         name="confirm_signin"),
 )
