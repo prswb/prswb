bed92ae81c4e3147687c216161c7eadbd4d77dd3
Lionel <lionel@soreia.com>
2012-09-30 13:01:09 +0200

    manage login required and confirm email

--- uxperiment/utils.py
+++ uxperiment/utils.py
@@ -1,5 +1,6 @@
+# coding: utf-8
 from django.core.mail import send_mass_mail
-from django.conf.settings import EMAIL_RECIPIENT, EMAIL_HOST_USER
+from django.conf import settings
 
 message = {
     "contact": '''\
@@ -26,35 +27,21 @@ L'équipe UXperiment
 ''',
 }
 
-def send_message(website, username, email):
-    subject = '    to_sender_message = 
-    to_admin_message = 'L''utilisateur : %s, vient de proposer le site %s'\
-        % (username, website)
-    to_sender = (subject, to_sender_message, 'no-reply@uxperiment.fr',
-            [email])
-    to_admin = (subject, to_admin_message, email,
-        [settings.EMAIL_RECIPIENT])
-    send_mass_mail((to_sender, to_admin))
-    
-def send_message(data,confirm):
-    to_sender_message = message[confirm]
-    to_admin_message = '    to_sender = (', to_sender_message[confirm],
-            'no-reply@uxperiment.fr', [data['sender']])
-    to_admin = (data['subject'], to_admin_message, data['sender'],
-        [settings.EMAIL_RECIPIENT])
-    send_mass_mail((build_user_email(confirm, data[, to_admin))
+def send_message(confirm, data):
+    send_mass_mail((build_user_email(confirm, data['sender']), 
+        build_admin_email(confirm, data)))
 
-def build_user_email(confirm, recipient)
+def build_user_email(confirm, recipient):
     """ Build user message in form of send_mail """
     text = message[confirm]
     if confirm == 'suggest':
         subject = 'Confirmation de votre proposition sur UXperiment'
     if confirm == 'contact':
         subject = 'Commentaire sur UXperiment'
-    if confirm = 'signin':
+    if confirm == 'signin':
         subject = 'Confirmation d\'inscription sur UXperiment'
-    
-    return (subject, text, EMAIL_HOST_USER, [recipient])
+
+    return subject, text, settings.EMAIL_HOST_USER, [recipient]
 
 def build_admin_email(confirm, data):
     """ Build admin message in form of send_mail """
@@ -66,12 +53,9 @@ def build_admin_email(confirm, data):
     if confirm == 'contact':
         subject = 'Nouveau contact sur UXperiment'
         text = '''\
-Adresse email : %s
+Email : %s
+Sujet : %s
+Message :
+%s''' % (data['sender'], data['subject'], data['message'])
 
----------------------------------------
-%s''' % (data['sender'], data['message'])
-
-    return (subject, text, EMAIL_HOST_USER, [EMAIL_RECIPIENT])
-
-
-
+    return subject, text, settings.EMAIL_HOST_USER, [settings.EMAIL_RECIPIENT]
