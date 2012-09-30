699c02afa497482465ffa77618db5deeff2bd083
Lionel <lionel@soreia.com>
2012-09-30 11:07:05 +0200

    test create user form post

--- registration/tests.py
+++ registration/tests.py
@@ -6,11 +6,23 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
+    
+    def setUp(self):
+        self.registred = User.objects.get(id=1)
+        self.Client = Client()
+
+    def test_user_creation(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests user creation
+        Response is redirected and new user is in database.
         """
-        self.assertEqual(1 + 1, 2)
+        datas = {"username": "test", "email": "plop@plop.com", 
+                "password1": "apassbutnotword", }
+        response = self.Client.post("/compte/creation/", datas)
+        print(response)
+        self.assertRedirects(response, "/compte/merci/")
