3afb29d1b82a2d10c5632ed460719a0a597f080f
Lionel <lionel@soreia.com>
2012-09-30 11:07:28 +0200

    add registration apps to installed apps

--- uxperiment/settings/base.py
+++ uxperiment/settings/base.py
@@ -117,6 +117,7 @@ INSTALLED_APPS = (
 #    'django.contrib.staticfiles',
     'pages',
     'websites',
+    'registration',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
