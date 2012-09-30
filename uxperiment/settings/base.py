6613915f997e3bfe71a2a6d7e2827c234b4ea2c9
Lionel <lionel@soreia.com>
2012-09-30 13:13:11 +0200

    add password hash in settings

--- uxperiment/settings/base.py
+++ uxperiment/settings/base.py
@@ -154,3 +154,12 @@ LOGGING = {
 }
 
 MARKDOWN_DIR = os.path.join(ROOT_DIR, 'markdown')
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+)
