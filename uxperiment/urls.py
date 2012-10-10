f5fdf9c9b728a4d7d1e885d6a3c007cd4803611c
Mathieu Tricoire <mathieu.tricoire@gmail.com>
2012-10-10 22:32:01 +0200

    Fixes issue 'explicit imports url' that fixes #4

--- uxperiment/urls.py
+++ uxperiment/urls.py
@@ -2,31 +2,39 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 
+# views and urls imports
+from pages import views as pages_views
+from websites import views as websites_views
+from registration import urls as registration_urls
+from django import views as django_views
+
+
 urlpatterns = patterns('',
-    url(r'^$', 'pages.views.dashboard', name='dashboard'),
-    url(r'^contact/$', 'pages.views.contact', name='contact'),
-    url(r'^contact/merci/$', 'pages.views.confirm_contact',
+    url(r'^$', pages_views.dashboard, name='dashboard'),
+    url(r'^contact/$', pages_views.contact, name='contact'),
+    url(r'^contact/merci/$', pages_views.confirm_contact,
         name='confirm_contact'),
 
     # Suggest a website
-    url(r'^proposer-un-site/$', 'websites.views.suggest',
+    url(r'^proposer-un-site/$', websites_views.suggest,
         name='suggest_website'),
-    url(r'^proposer-un-site/merci/$', 'websites.views.confirm_suggest',
+    url(r'^proposer-un-site/merci/$', websites_views.confirm_suggest,
         name='confirm_suggest_website'),
-    url(r'^informations-sur-un-site/$', 'websites.views.informations',
+    url(r'^informations-sur-un-site/$', websites_views.informations,
         name='informations_website'),
 
-    url(r'^pages/(?P<slug>[-\w\d]+)/$', 'pages.views.markdown_page',
+    url(r'^pages/(?P<slug>[-\w\d]+)/$', pages_views.markdown_page,
         name='markdown_page'),
-    url(r'^compte/', include('registration.urls')),
+    url(r'^compte/', include(registration_urls)),
 )
+
 
 # local dev
 urlpatterns += patterns('',
-     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+     url(r'^static/(?P<path>.*)$', django_views.static.serve, {
             'document_root': settings.STATIC_ROOT,
         }),
-     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+     url(r'^media/(?P<path>.*)$', django_views.static.serve, {
             'document_root': settings.MEDIA_ROOT,
         }),
 )
