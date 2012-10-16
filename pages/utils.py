8840fd12f7347ba9cc892492969920a5ad168324
Nicolas Perriault <nperriault@gmail.com>
2012-10-16 16:17:32 +0200

    removed overengineered defensive programming attempt

--- pages/utils.py
+++ pages/utils.py
@@ -3,12 +3,10 @@ import os
 from django.conf import settings
 
 
-def resolve_markdown_path(slug, lang=None):
+def resolve_markdown_path(slug, lang):
     """ Retrieves an existing markdown file path from slug and a language code.
         Returns None in case no matching file was found.
     """
-    if lang is None:
-        return slug
     i18n_filename = os.path.join(settings.MARKDOWN_DIR, lang, '%s.md' % slug)
     filename = os.path.join(settings.MARKDOWN_DIR, '%s.md' % slug)
     if os.path.isfile(i18n_filename):
