9799ba02868c41f34a643bb1d7ee3d8b27c0d441
Vincent Agnano <vincent.agnano@scopyleft.fr>
2012-10-16 16:19:47 +0200

    refactored utils sendmail

--- uxperiment/utils.py
+++ uxperiment/utils.py
@@ -1,47 +1,26 @@
 # coding: utf-8
 from django.core.mail import send_mass_mail
 from django.conf import settings
+from django.template.loader import get_template
 
-message = {
-    "contact": '''\
-Bonjour,
+from django.template import Context
 
-Votre commentaire sur le site www.uxperiment.fr a bien été pris en compte.
-
-Nous vous remercions de votre participation.
-
-A bientôt,
-
-L'équipe UXperiment
-''',
-    "suggest": '''\
-Bonjour,
-
-Votre proposition de site internet sur le site www.uxperiment.fr a bien été pris en compte.
-
-Nous vous remercions de votre participation.
-
-A bientôt,
-
-L'équipe UXperiment
-''',
-}
 
 def send_message(confirm, data):
-    send_mass_mail((build_user_email(confirm, data['sender']), 
+    send_mass_mail((build_user_email(confirm, data['sender']),
         build_admin_email(confirm, data)))
 
 def build_user_email(confirm, recipient):
     """ Build user message in form of send_mail """
-    text = message[confirm]
-    if confirm == 'suggest':
-        subject = 'Confirmation de votre proposition sur UXperiment'
-    if confirm == 'contact':
-        subject = 'Commentaire sur UXperiment'
-    if confirm == 'signin':
-        subject = 'Confirmation d\'inscription sur UXperiment'
+    subjects = (
+        ('suggest', "Confirmation de votre proposition sur UXperiment"),
+        ('contact', "Commentaire sur UXperiment"),
+        ('signin', "Confirmation d'inscription sur UXperiment"),
+        )
+    subject = dict(subjects).get(confirm)
+    message = get_template('mails/%s.txt' % confirm).render(Context())
 
-    return subject, text, settings.EMAIL_HOST_USER, [recipient]
+    return subject, message, settings.EMAIL_HOST_USER, [recipient]
 
 def build_admin_email(confirm, data):
     """ Build admin message in form of send_mail """
@@ -50,7 +29,7 @@ def build_admin_email(confirm, data):
         text = 'L\'utilisateur : %s, vient de proposer le site %s'\
         % (data['username'], data['website'])
 
-    if confirm == 'contact':
+    elif confirm == 'contact':
         subject = 'Nouveau contact sur UXperiment'
         text = '''\
 Email : %s
