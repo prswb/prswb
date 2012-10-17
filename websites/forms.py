0ce1150613b211ffa1b080e6f90d023966bf69e5
Nicolas Perriault <nperriault@gmail.com>
2012-10-17 21:40:56 +0200

    removed website picture upload field from form
    Because Heroku only provided an ephemeral filesystem, we can't rely
    on user uploads.

--- websites/forms.py
+++ websites/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from websites.models import Website
 
 class SuggestForm(forms.ModelForm):
+    """ Picture field is excluded on purpose, as Heroku provided an ephemeral
+        filesystem.
+    """
     class Meta:
         model = Website
-        exclude = ('submitter',)
+        exclude = ('picture', 'submitter',)
