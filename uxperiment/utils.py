ae1ac6f150877e53d476779459952e23b378c200
Vincent Agnano <vincent.agnano@scopyleft.fr>
2012-10-18 12:09:16 +0200

    added translated emails

--- uxperiment/utils.py
+++ uxperiment/utils.py
@@ -2,7 +2,7 @@
 from django.core.mail import send_mass_mail
 from django.conf import settings
 from django.template.loader import get_template
-from django.utils.translation import ugettext as _
+from django.utils.translation import get_language, ugettext as _
 
 
 from django.template import Context
@@ -21,7 +21,9 @@ def build_user_email(confirm, recipient):
         ('signin', _("Confirmation d'inscription sur UXperiment")),
         )
     subject = dict(subjects).get(confirm)
-    message = get_template('mails/%s.txt' % confirm).render(Context())
+    lang = get_language()[0:2]
+    message = get_template('mails/%s/%s.txt' % (lang, confirm))\
+                .render(Context())
 
     return subject, message, settings.EMAIL_HOST_USER, [recipient]
 
